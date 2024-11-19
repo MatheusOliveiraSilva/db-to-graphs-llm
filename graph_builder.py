f065f5b8da751e594cfc85d9a728328700c6cf8a
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2024-11-19 16:44:55 -0300

    v0 100%

--- graph_builder.py
+++ graph_builder.py
@@ -21,27 +21,29 @@ class DBToGraph:
         self.llm = ChatOpenAI(temperature=0, model_name="gpt-4o")
 
         # To use the neo4j graph, need to run neo4j.sh script. (for more info see README.md)
-        self.graph = Neo4jGraph(refresh_schema=False)
-
-        # Chunk the schema into langchain documents for each "CREATE TABLE".
-        self.schema_documents = self.transform_schema_to_langchain_documents(schema)
+        self.graph = Neo4jGraph(refresh_schema=True)
 
         # Initialize the LLMGraphTransformer with the DB_GRAPH_PROMPT and create KB, after this send for Neo4j.
         self.llm_transformer = self.initialize_db_graph_transformer()
-        self.kb = self.create_db_graph()
-        self.send_to_neo4j()
+
+        # Augment the table infos with the LLM model and make langchain documents about it.
+        self.augmented_schema_chunks = self.transform_schema_to_langchain_documents(schema)
 
     def initialize_db_graph_transformer(self):
         return LLMGraphTransformer(
             llm=self.llm,
             prompt=DB_GRAPH_PROMPT,
             allowed_nodes=["TABLE", "COLUMN"],
-            allowed_relationships=["IS_COLUMN_OF", "FOREIGN_KEY_TO", "RELATES_TO"],
+            allowed_relationships=["IS_COLUMN_OF", "FOREIGN_KEY_OF"],
             node_properties=[
                 "table_name", "columns", "primary_key", "foreign_keys", "table_description",
                 "column_name", "data_type", "is_nullable", "is_primary_key", "is_foreign_key", "column_description"
             ]
         )
+
+    def augment_table_infos(self, table_infos):
+        chain = DATA_AUGMENTATION_PROMPT | self.llm | StrOutputParser()
+        return chain.invoke({"input_schema": table_infos})
 
     def transform_schema_to_langchain_documents(self, schema):
         langchain_documents = []
@@ -56,28 +58,23 @@ class DBToGraph:
                 for table in tables:
                     augmented_schema_chunk = self.augment_table_infos(f"CREATE TABLE {table}")
                     file.write(augmented_schema_chunk)
-                    file.write("\n---chunk---\n")
+                    file.write("\n---new-table-chunk---\n")
                     langchain_documents.append(Document(page_content=augmented_schema_chunk))
-                    print(f"Just created chunk:", augmented_schema_chunk)
         else:
             with open(augmented_infos_path, "r") as file:
                 augmented_schema_chunk = file.read()
-                langchain_documents.append(Document(page_content=augmented_schema_chunk))
-                print(f"Just readed chunk:", augmented_schema_chunk)
+                chunks = augmented_schema_chunk.split("\n---new-table-chunk---\n")
+                for chunk in chunks:
+                    langchain_documents.append(Document(page_content=chunk))
         return langchain_documents
 
-    def create_db_graph(self):
-        langchain_documents = self.schema_documents
-        graph_from_docs = self.llm_transformer.convert_to_graph_documents(langchain_documents)
-        return graph_from_docs
-
-    def send_to_neo4j(self):
-        self.graph.add_graph_documents(self.kb, include_source=True)
-
-    def augment_table_infos(self, table_infos):
-        chain = DATA_AUGMENTATION_PROMPT | self.llm | StrOutputParser()
-        return chain.invoke({"input_schema": table_infos})
+    def create_kg(self):
+        for chunk in self.augmented_schema_chunks:
+            print("Chunk:", chunk)
+            graph_from_docs = self.llm_transformer.convert_to_graph_documents([chunk])
+            self.graph.add_graph_documents(graph_from_docs, include_source=True)
 
 if __name__ == "__main__":
     db_to_graph = DBToGraph(MONDIAL_SCHEMA)
+    db_to_graph.create_kg()
     print("Graph created successfully!")
