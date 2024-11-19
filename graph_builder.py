f91bdf6b4ca3ab3e871caf4f088d17dcd725e1dc
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2024-11-19 11:00:55 -0300

    teste 1 - tese

--- graph_builder.py
+++ graph_builder.py
@@ -13,33 +13,51 @@ from schemas.mondial_schema import MONDIAL_SCHEMA
 
 load_dotenv()
 
-llm = ChatOpenAI(temperature=0, model_name="gpt-4o")
-llm_transformer = LLMGraphTransformer(llm=llm)
+class DBToGraph:
+    def __init__(self, schema):
+        # Modify temperature and model_name experimentally if need some improvements.
+        self.llm = ChatOpenAI(temperature=0.7, model_name="gpt-4o")
 
-graph = Neo4jGraph(refresh_schema=True)
+        # To use the neo4j graph, need to run neo4j.sh script. (for more info see README.md)
+        self.graph = Neo4jGraph()
 
-def initialize_db_graph_transformer():
-    return LLMGraphTransformer(
-        llm=llm,
-        prompt=DB_GRAPH_PROMPT,
-        allowed_nodes=["TABLE", "COLUMN"],
-        allowed_relationships=["HAS_COLUMN", "FOREIGN_KEY_TO", "RELATES_TO"],
-        #node_properties=[
-        #    "table_name", "columns", "primary_key", "foreign_keys", "table_description",
-        #    "column_name", "data_type", "is_nullable", "is_primary_key", "is_foreign_key", "column_description"
-        #]
-    )
+        # Chunk the schema into langchain documents for each "CREATE TABLE".
+        self.schema_documents = self.transform_schema_to_langchain_documents(schema)
 
-def create_db_graph(db_schema, llm_transformer):
-    langchain_documents = [Document(page_content=db_schema)]
-    graph_from_docs = llm_transformer.convert_to_graph_documents(langchain_documents)
-    return graph_from_docs
+        # Initialize the LLMGraphTransformer with the DB_GRAPH_PROMPT and create KB, after this send for Neo4j.
+        self.llm_transformer = self.initialize_db_graph_transformer()
+        self.kb = self.create_db_graph()
+        self.send_to_neo4j()
 
-def send_to_neo4j(graph, kb):
-    graph.add_graph_documents(kb, include_source=True)
+    def initialize_db_graph_transformer(self):
+        return LLMGraphTransformer(
+            llm=self.llm,
+            prompt=DB_GRAPH_PROMPT,
+            allowed_nodes=["TABLE", "COLUMN"],
+            allowed_relationships=["HAS_COLUMN", "FOREIGN_KEY_TO", "RELATES_TO"],
+            node_properties=[
+                "table_name", "columns", "primary_key", "foreign_keys", "table_description",
+                "column_name", "data_type", "is_nullable", "is_primary_key", "is_foreign_key", "column_description"
+             ]
+        )
+
+    @staticmethod
+    def transform_schema_to_langchain_documents(schema):
+        langchain_documents = []
+
+        tables = schema.split("CREATE TABLE")
+        for table in tables:
+            langchain_documents.append(Document(page_content=f"CREATE TABLE {table}"))
+        return langchain_documents
+
+    def create_db_graph(self):
+        langchain_documents = self.schema_documents
+        graph_from_docs = self.llm_transformer.convert_to_graph_documents(langchain_documents)
+        return graph_from_docs
+
+    def send_to_neo4j(self):
+        self.graph.add_graph_documents(self.kb, include_source=True)
 
 if __name__ == "__main__":
-    db_schema = MONDIAL_SCHEMA
-    db_graph_builder = initialize_db_graph_transformer()
-    kb = create_db_graph(db_schema, db_graph_builder)
-    send_to_neo4j(graph, kb)
+    db_to_graph = DBToGraph(MONDIAL_SCHEMA)
+    print("Graph created successfully!")
